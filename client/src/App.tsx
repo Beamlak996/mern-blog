58178576ff71ca3e05ffa83bc0d37e6a8f563af2
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-07-12 06:01:30 -0900

    make the dashboard private

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { Home } from "./pages/home"
-import { About } from "./pages/about"
-import { SignIn } from "./pages/signin"
-import { SignUp } from "./pages/signup"
-import { Dashobard } from "./pages/dashboard"
-import { Projects } from "./pages/projects"
-import { Header } from "./components/header"
-import { FooterCom } from "./components/footer"
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+import { SignIn } from "./pages/signin";
+import { SignUp } from "./pages/signup";
+import { Dashobard } from "./pages/dashboard";
+import { Projects } from "./pages/projects";
+import { Header } from "./components/header";
+import { FooterCom } from "./components/footer";
+import PrivateRoute from "./components/private-route";
 
 function App() {
   return (
@@ -18,12 +19,14 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashobard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashobard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterCom />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
